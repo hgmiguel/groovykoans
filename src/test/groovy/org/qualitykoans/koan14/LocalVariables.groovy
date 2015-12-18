3bcc02e6d8af410d88298cbd0e695bc603c5e56a
diegoazd <diegoazd@hotmail.com>
2015-12-17 21:35:04 -0600

    Extract method and local variable

--- src/test/groovy/org/qualitykoans/koan14/LocalVariables.groovy
+++ src/test/groovy/org/qualitykoans/koan14/LocalVariables.groovy
@@ -13,15 +13,19 @@ class LocalVariables {
     e.each { Order each ->
       outstanding += each.getAmount();
     }
-    //print details
-    System.out.println ("name:" + _name);
-    System.out.println ("amount:" + outstanding);
+
+    printDetails(outstanding);
   }
 
   public void printBanner() {
     System.out.println ("**************************");
     System.out.println ("***** Customer Owes ******");
     System.out.println ("**************************");
+  }
+
+  public void printDetails(double outstanding) {
+    System.out.println ("name:" + _name);
+    System.out.println ("amount:" + outstanding);
   }
 
 }
